f48d615f04ef22e57a20ab15bc639c81804ab71f
Nick Blomberg <nick.blomberg@gmail.com>
2016-07-16 12:14:13 +0100

    Improve main script by allowing individual tests to be run

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
 import unittest
-from test import LoginTestCase, DashboardTestCase, ProjectFormTestCase
-
-all_tests = unittest.TestSuite([
-    unittest.TestLoader().loadTestsFromTestCase(LoginTestCase),
-    unittest.TestLoader().loadTestsFromTestCase(DashboardTestCase),
-    unittest.TestLoader().loadTestsFromTestCase(ProjectFormTestCase),
-])
+from test import *
 
 if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=3).run(all_tests)
+    unittest.main(verbosity=3)
